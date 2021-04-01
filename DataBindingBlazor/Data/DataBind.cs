54eb67c3184a423c3060a69afa6e6e8b403b73b4
Jay Krishna Reddy <majji.jaya_krishna_reddy@nokia.com>
2021-04-01 18:43:48 +0530

    Add project files.

---
+++ DataBindingBlazor/Data/DataBind.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataBindingBlazor.Data
+{
+    public class DataBind : ComponentBase
+    {
+        public string Name = "Jay krishna Reddy"; 
+    }
+}
